9a578acf46a03b931ccf81e69721a529d4edeaa0
Filipe Silva <filipeqs@outlook.com>
2022-09-30 16:53:39 -0400

    Exercises Controller Initial

---
+++ Gymate/Services/Exercise/Exercise.Api/Controllers/ExercisesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercise.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExercisesController : ControllerBase
+    {
+    }
+}
